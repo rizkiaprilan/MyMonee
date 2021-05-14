168df26a136f0b95c919a4d984a2ab7cb7712fe4
Rizki Aprilan <riskiazza@gmail.com>
2021-05-14 21:08:45 +0700

    update

---
+++ MyMonee-Final/Controllers/UpdateImpianViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UpdateImpianViewController.swift
+//  MyMonee-Final
+//
+//  Created by Muhammad Rizki Aprilan on 14/05/21.
+//
+
+import UIKit
+
+class UpdateImpianViewController: UIViewController {
+
+    @IBOutlet weak var deleteButton: UIButton!
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        deleteButton.layer.borderWidth = CGFloat(3)
+//        deleteButton.layer.backgroundColor = CGColor(red: 80, green: 105, blue: 184, alpha: 1)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
